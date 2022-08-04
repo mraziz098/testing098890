53d989916750d1b108bc2faf2894f3743c37703d
Muhammad Rafi Aziz <53348419+mraziz098@users.noreply.github.com>
2022-08-04 21:17:30 +0700

    2
    2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(href="https://storage.googleapis.com/flowery-351313/project%20101.html")
+    res.redirect('https://storage.googleapis.com/flowery-351313/Portfolio.html')
 })
 
 const PORT = process.env.PORT || 8000
